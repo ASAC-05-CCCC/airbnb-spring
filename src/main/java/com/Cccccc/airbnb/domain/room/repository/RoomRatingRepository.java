983eaa8da3e4b43a48cd2b4e8e46fac7b8e8b958
IforCU <tmdgus0844@gmail.com>
2024-06-26 14:28:26 +0900

    Feat:누락된 Repository

--- src/main/java/com/Cccccc/airbnb/domain/room/repository/RoomRatingRepository.java
+++ src/main/java/com/Cccccc/airbnb/domain/room/repository/RoomRatingRepository.java
@@ -3,5 +3,5 @@ package com.Cccccc.airbnb.domain.room.repository;
 import com.Cccccc.airbnb.domain.entity.RoomRating;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoomRatingRepository extends JpaRepository<RoomRating, Integer> {
+public interface RoomRatingRepository  extends JpaRepository<RoomRating,Integer> {
 }
