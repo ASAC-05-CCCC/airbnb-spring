0b8f64803422c051d287a3c7b5e93b727e517451
IforCU <tmdgus0844@gmail.com>
2024-06-26 15:24:12 +0900

    Refactor: 불필요한 코드 삭제

--- src/main/java/com/Cccccc/airbnb/domain/room/controller/RoomController.java
+++ src/main/java/com/Cccccc/airbnb/domain/room/controller/RoomController.java
@@ -3,13 +3,12 @@ package com.Cccccc.airbnb.domain.room.controller;
 import com.Cccccc.airbnb.domain.room.dto.HostResponseDto;
 import com.Cccccc.airbnb.domain.room.service.RoomService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
+
 @RestController
 @RequestMapping("/api/c2/room")
 @RequiredArgsConstructor
