398e55cd77f4f68b8b1e8d88eff08ebfc555aea5
IforCU <tmdgus0844@gmail.com>
2024-06-26 13:43:29 +0900

    Feat: Host API

--- src/main/java/com/Cccccc/airbnb/domain/room/controller/RoomController.java
+++ src/main/java/com/Cccccc/airbnb/domain/room/controller/RoomController.java
@@ -3,11 +3,13 @@ package com.Cccccc.airbnb.domain.room.controller;
 import com.Cccccc.airbnb.domain.room.dto.HostResponseDto;
 import com.Cccccc.airbnb.domain.room.service.RoomService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/c2/room")
 @RequiredArgsConstructor
@@ -17,6 +19,6 @@ public class RoomController {
 
     @GetMapping("/host/{id}")
     public HostResponseDto getHostByRoomId(@PathVariable Integer id) {
-        return roomService.getUserByRoomId(id);
+        return roomService.getHostByRoomId(id);
     }
 }
