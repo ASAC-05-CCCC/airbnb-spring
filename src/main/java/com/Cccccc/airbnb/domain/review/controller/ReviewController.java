fcd62e6425ff339956fed83fb2f69ac5e80e37a5
yunhyungNAM <iys04003@naver.com>
2024-06-30 23:05:41 +0900

    Add: apis for POST, PATCH and DELETE

--- src/main/java/com/Cccccc/airbnb/domain/review/controller/ReviewController.java
+++ src/main/java/com/Cccccc/airbnb/domain/review/controller/ReviewController.java
@@ -1,13 +1,11 @@
 package com.Cccccc.airbnb.domain.review.controller;
 
-
+import com.Cccccc.airbnb.domain.review.dto.ReviewCreateDto;
 import com.Cccccc.airbnb.domain.review.dto.ReviewResponseDto;
+import com.Cccccc.airbnb.domain.review.dto.ReviewUpdateDto;
 import com.Cccccc.airbnb.domain.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,10 +13,25 @@ import java.util.List;
 @RequestMapping("/api/c2/review")
 @RequiredArgsConstructor
 public class ReviewController {
-    private final ReviewService ReviewService;
+    private final ReviewService reviewService;
 
     @GetMapping("/{id}")
-    public List<ReviewResponseDto> getReviewByRoomId(@PathVariable Integer id){
-        return ReviewService.getReviewByRoomId(id);
+    public List<ReviewResponseDto> getReviewByRoomId(@PathVariable Integer id) {
+        return reviewService.getReviewByRoomId(id);
+    }
+
+    @PostMapping("/add")
+    public ReviewResponseDto create(@RequestBody ReviewCreateDto request) {
+        return reviewService.create(request);
+    }
+
+    @PatchMapping("/{id}/fix")
+    public ReviewResponseDto update(@PathVariable Integer id, @RequestBody ReviewUpdateDto request) {
+        return reviewService.update(id, request);
+    }
+
+    @DeleteMapping("/{id}/delete")
+    public Integer delete(@PathVariable Integer id) {
+        return reviewService.delete(id);
     }
 }
